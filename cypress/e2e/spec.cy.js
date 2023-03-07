64026f5048cdfe6cd0c76f3e92e6e8b2cf21e107
Robert Aiken <robertsiraaiken@gmail.com>
2023-03-07 12:24:41 -0600

    Add more specific test for iteration4

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -33,12 +33,12 @@ describe('empty spec', () => {
   })
 
   it('should display the new shortened url after the form is submitted',()=>{
-    cy.intercept('POST','http://localhost:3001/api/v1/urls',{id: 2, long_url: "https://images.unsplash.com/photo...", short_url: "http://localhost:3001/useshorturl/2", title: 'Awesome photo'}).as('apiCheck')
+    cy.intercept('POST','http://localhost:3001/api/v1/urls',{id: 2, long_url: "This has been stubbed", short_url: "http://localhost:3001/useshorturl/2", title: 'Congrats'}).as('apiCheck')
     cy.get('input[name="title"]')
     .type("This will")
     .get('input[name="urlToShorten"]')
     .type("Get stubbed")
     .get('form').children().last().click()
-
+    .get('div[class="url"]').contains('Congrats')
   })
 })
